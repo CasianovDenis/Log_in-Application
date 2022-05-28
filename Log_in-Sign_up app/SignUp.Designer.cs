7a1db9e8d42390497051b9a7badd2d0096fab84e
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-05-28 18:44:52 +0300

    Create fuction,which verified format email,password and verified if already exist date in database,which specified in the field

--- Log_in-Sign_up app/SignUp.Designer.cs
+++ Log_in-Sign_up app/SignUp.Designer.cs
@@ -32,6 +32,7 @@ namespace Log_in_Sign_up_app
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Label usernameLabel;
             System.Windows.Forms.Label passwordLabel;
+            System.Windows.Forms.Label label2;
             this.create_acc_button = new System.Windows.Forms.Button();
             this.signUp_LogInDataSet = new Log_in_Sign_up_app.SignUp_LogInDataSet();
             this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -41,8 +42,14 @@ namespace Log_in_Sign_up_app
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.exit_button = new System.Windows.Forms.Button();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
+            this.incorect_format_label = new System.Windows.Forms.Label();
+            this.username_eror_label = new System.Windows.Forms.Label();
+            this.password_eror_label = new System.Windows.Forms.Label();
+            this.exist_email_label = new System.Windows.Forms.Label();
             usernameLabel = new System.Windows.Forms.Label();
             passwordLabel = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.signUp_LogInDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -51,7 +58,7 @@ namespace Log_in_Sign_up_app
             // 
             usernameLabel.AutoSize = true;
             usernameLabel.ForeColor = System.Drawing.Color.White;
-            usernameLabel.Location = new System.Drawing.Point(106, 63);
+            usernameLabel.Location = new System.Drawing.Point(116, 189);
             usernameLabel.Name = "usernameLabel";
             usernameLabel.Size = new System.Drawing.Size(58, 13);
             usernameLabel.TabIndex = 12;
@@ -61,17 +68,27 @@ namespace Log_in_Sign_up_app
             // 
             passwordLabel.AutoSize = true;
             passwordLabel.ForeColor = System.Drawing.Color.White;
-            passwordLabel.Location = new System.Drawing.Point(108, 115);
+            passwordLabel.Location = new System.Drawing.Point(118, 272);
             passwordLabel.Name = "passwordLabel";
             passwordLabel.Size = new System.Drawing.Size(56, 13);
             passwordLabel.TabIndex = 13;
             passwordLabel.Text = "Password:";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = System.Drawing.Color.White;
+            label2.Location = new System.Drawing.Point(116, 83);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(35, 13);
+            label2.TabIndex = 18;
+            label2.Text = "Email:";
+            // 
             // create_acc_button
             // 
             this.create_acc_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.create_acc_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.create_acc_button.Location = new System.Drawing.Point(111, 167);
+            this.create_acc_button.Location = new System.Drawing.Point(116, 356);
             this.create_acc_button.Name = "create_acc_button";
             this.create_acc_button.Size = new System.Drawing.Size(78, 36);
             this.create_acc_button.TabIndex = 11;
@@ -102,18 +119,18 @@ namespace Log_in_Sign_up_app
             // usernameTextBox
             // 
             this.usernameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBindingSource, "Username", true));
-            this.usernameTextBox.Location = new System.Drawing.Point(170, 60);
+            this.usernameTextBox.Location = new System.Drawing.Point(180, 186);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.Size = new System.Drawing.Size(141, 20);
             this.usernameTextBox.TabIndex = 13;
             // 
             // passwordTextBox
             // 
             this.passwordTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tableBindingSource, "Password", true));
-            this.passwordTextBox.Location = new System.Drawing.Point(170, 112);
+            this.passwordTextBox.Location = new System.Drawing.Point(180, 269);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(141, 20);
             this.passwordTextBox.TabIndex = 14;
             // 
             // label1
@@ -132,7 +149,7 @@ namespace Log_in_Sign_up_app
             // 
             this.exit_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.exit_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exit_button.Location = new System.Drawing.Point(250, 174);
+            this.exit_button.Location = new System.Drawing.Point(246, 356);
             this.exit_button.Name = "exit_button";
             this.exit_button.Size = new System.Drawing.Size(75, 23);
             this.exit_button.TabIndex = 16;
@@ -140,12 +157,69 @@ namespace Log_in_Sign_up_app
             this.exit_button.UseVisualStyleBackColor = true;
             this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
             // 
+            // emailtextBox
+            // 
+            this.emailtextBox.Location = new System.Drawing.Point(180, 80);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(141, 20);
+            this.emailtextBox.TabIndex = 17;
+            // 
+            // incorect_format_label
+            // 
+            this.incorect_format_label.AutoSize = true;
+            this.incorect_format_label.ForeColor = System.Drawing.Color.Red;
+            this.incorect_format_label.Location = new System.Drawing.Point(177, 116);
+            this.incorect_format_label.Name = "incorect_format_label";
+            this.incorect_format_label.Size = new System.Drawing.Size(117, 13);
+            this.incorect_format_label.TabIndex = 19;
+            this.incorect_format_label.Text = "Format email is incorect";
+            this.incorect_format_label.Visible = false;
+            // 
+            // username_eror_label
+            // 
+            this.username_eror_label.AutoSize = true;
+            this.username_eror_label.ForeColor = System.Drawing.Color.Red;
+            this.username_eror_label.Location = new System.Drawing.Point(177, 225);
+            this.username_eror_label.Name = "username_eror_label";
+            this.username_eror_label.Size = new System.Drawing.Size(137, 13);
+            this.username_eror_label.TabIndex = 20;
+            this.username_eror_label.Text = "This username already exist";
+            this.username_eror_label.Visible = false;
+            // 
+            // password_eror_label
+            // 
+            this.password_eror_label.AutoSize = true;
+            this.password_eror_label.ForeColor = System.Drawing.Color.Red;
+            this.password_eror_label.Location = new System.Drawing.Point(82, 311);
+            this.password_eror_label.Name = "password_eror_label";
+            this.password_eror_label.Size = new System.Drawing.Size(334, 13);
+            this.password_eror_label.TabIndex = 21;
+            this.password_eror_label.Text = "Password is not secure,needed 6+ word,uppercase word and number";
+            this.password_eror_label.Visible = false;
+            // 
+            // exist_email_label
+            // 
+            this.exist_email_label.AutoSize = true;
+            this.exist_email_label.ForeColor = System.Drawing.Color.Red;
+            this.exist_email_label.Location = new System.Drawing.Point(177, 154);
+            this.exist_email_label.Name = "exist_email_label";
+            this.exist_email_label.Size = new System.Drawing.Size(115, 13);
+            this.exist_email_label.TabIndex = 22;
+            this.exist_email_label.Text = "This email already exist";
+            this.exist_email_label.Visible = false;
+            // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(425, 450);
+            this.Controls.Add(this.exist_email_label);
+            this.Controls.Add(this.password_eror_label);
+            this.Controls.Add(this.username_eror_label);
+            this.Controls.Add(this.incorect_format_label);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.emailtextBox);
             this.Controls.Add(this.exit_button);
             this.Controls.Add(this.label1);
             this.Controls.Add(passwordLabel);
@@ -173,5 +247,10 @@ namespace Log_in_Sign_up_app
         private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.TextBox emailtextBox;
+        private System.Windows.Forms.Label incorect_format_label;
+        private System.Windows.Forms.Label username_eror_label;
+        private System.Windows.Forms.Label password_eror_label;
+        private System.Windows.Forms.Label exist_email_label;
     }
 }
--- Log_in-Sign_up app/SignUp.cs
+++ Log_in-Sign_up app/SignUp.cs
@@ -23,31 +23,35 @@ namespace Log_in_Sign_up_app
 
         private void SignUp_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void create_acc_button_Click(object sender, EventArgs e)
         {
-            Boolean flag = false;
+            bool flag=false;
 
             con.Open();
 
-            //try get exist username
-            try
+            if (existemail() == true)
             {
+                incorect_format_label.Visible = false;
+                exist_email_label.Visible = false;
+                
 
-                SqlCommand cmd = new SqlCommand("INSERT INTO MyTable(ID,Username,Password) values('" + null + "','" + usernameTextBox.Text + "','" + passwordTextBox.Text + "')", con);
-                cmd.ExecuteNonQuery();
-                flag = true;
+                if (securepass() == true)
+                {
 
+                    if (existusername() == true)
+                    {
+
+                        SqlCommand cmd = new SqlCommand("INSERT INTO MyTable(ID,Email,Username,Password) values('" + null + "','" + emailtextBox.Text + "','" + usernameTextBox.Text + "','" + passwordTextBox.Text + "')", con);
+                        cmd.ExecuteNonQuery();
+                        flag = true;
+                    }
+
+                }
             }
-            catch
-            {
-                MessageBox.Show("This username already exist");
-                usernameTextBox.Clear();
-                passwordTextBox.Clear();
-                flag = false;
-            }
+
 
             con.Close();
 
@@ -62,6 +66,113 @@ namespace Log_in_Sign_up_app
         private void exit_button_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private bool existemail()
+        {
+            bool flag = false;
+            //try get exist username or email
+            string text = emailtextBox.Text;
+            char[] email = text.ToCharArray();
+            for (int index = 0; index < email.Length; index++)
+            {
+                if (email[index] == '@')
+                {
+
+                    string query_email = string.Format("select email from MyTable " +
+              "where  email='{0}'", emailtextBox.Text);
+
+                    SqlCommand cmd1 = new SqlCommand(query_email, con);
+                    SqlDataReader reader1 = cmd1.ExecuteReader();
+                    if (reader1.Read() == true)
+                    {
+                        //get error if exist this email in database
+
+                        exist_email_label.Visible = true;
+                        flag = false;
+
+                    }
+                    else
+                    {
+                        exist_email_label.Visible = false;
+                        flag = true;
+                        reader1.Close();
+                        cmd1.Cancel();
+                        
+                    }
+
+
+                }
+                else
+                {
+                    if (exist_email_label.Visible == true) incorect_format_label.Visible = false;
+                    else
+                        incorect_format_label.Visible = true;
+                }
+
+            }
+            return flag;
+        }
+
+        private bool securepass()
+        {
+            bool flag = false;
+            int count = 0;
+            string text = passwordTextBox.Text;
+            char[] pass = text.ToCharArray();
+            if (pass.Length >= 6)
+            {
+                for (int index = 0; index < pass.Length; index++)
+                {
+                    if (Char.IsUpper(pass[index]) == true)
+                    {
+                        count++;
+                    }
+
+                    else
+                         if (Char.IsNumber(pass[index]) == true)
+                    {
+                        count++;
+                    }
+                }
+            }
+            else
+            if (pass.Length < 6)
+            {
+                flag = false;
+                password_eror_label.Visible = true;
+            }
+
+            if (count >= 2) { password_eror_label.Visible = false; flag = true; return flag; }
+            else
+            {
+                password_eror_label.Visible = true;
+                flag = false; return flag;
+            }
+        }
+
+        private bool existusername()
+        {
+            bool flag = false;
+
+            string query_username = string.Format("select username from MyTable " +
+              "where username='{0}'", usernameTextBox.Text);
+
+            SqlCommand cmd2 = new SqlCommand(query_username, con);
+            SqlDataReader reader2 = cmd2.ExecuteReader();
+            if (reader2.Read() == true)
+            {
+                flag = false;
+                username_eror_label.Visible = true;
+            }
+            else
+            {
+                flag = true;
+                username_eror_label.Visible = false;
+                reader2.Close();
+                cmd2.Cancel();
+            }
+            return flag;
         }
     }
 }
