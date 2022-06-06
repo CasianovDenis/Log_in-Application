0f956e5263bfcb4c6d8c7cfd05822636a097e1b2
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-06-06 19:06:45 +0300

    Added function send message to user email,which insert when register your account,and this message has user password,if he forgot it

--- Log_in-Sign_up app/ForgotPass.cs
+++ Log_in-Sign_up app/ForgotPass.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Net.Mail;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+
 
 namespace Log_in_Sign_up_app
 {
@@ -36,12 +39,25 @@ namespace Log_in_Sign_up_app
             SqlDataReader reader = cmd.ExecuteReader();
             if (reader.Read() == true)
             {
-               display_pass_label.Text="You password: " + reader.GetString(0);
-                display_pass_label.Visible = true;
+                SmtpClient Smtp = new SmtpClient("smtp.mail.ru", 587);
+                Smtp.EnableSsl = true;
+                Smtp.Credentials = new NetworkCredential("email@mail.ru", "password");
+                
+                MailMessage Message = new MailMessage();
+                Message.From = new MailAddress("email@mail.ru");
+                Message.To.Add(new MailAddress(emailtextBox.Text));
+                Message.Subject = "Restore password";
+                Message.Body = "Password:"+reader.GetString(0)+"\n"+
+                    "If is not you request,please change password in application.";
 
+                  Smtp.Send(Message);
+
+                    MessageBox.Show("You password was sent to email");
+                this.Close();
             }
             else
-                MessageBox.Show("Incorect email or account not created");
+            MessageBox.Show("Incorect email");
+            
             con.Close();
         }
 
@@ -49,5 +65,7 @@ namespace Log_in_Sign_up_app
         {
             this.Close();
         }
+
+        
     }
 }
