bb0f5199cda797633ed41ec454e28102d4ac87e8
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-06-15 19:41:45 +0300

    modifie design

--- Log_in-Sign_up app/Change_email.Designer.cs
+++ Log_in-Sign_up app/Change_email.Designer.cs
@@ -36,11 +36,15 @@ namespace Log_in_Sign_up_app
             this.old_email_textBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.Confirme_textBox = new System.Windows.Forms.TextBox();
+            this.confirmelabel = new System.Windows.Forms.Label();
+            this.Confirme_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // exist_email_label
             // 
             this.exist_email_label.AutoSize = true;
+            this.exist_email_label.ForeColor = System.Drawing.Color.Red;
             this.exist_email_label.Location = new System.Drawing.Point(108, 155);
             this.exist_email_label.Name = "exist_email_label";
             this.exist_email_label.Size = new System.Drawing.Size(115, 13);
@@ -61,7 +65,7 @@ namespace Log_in_Sign_up_app
             // 
             this.change_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.change_button.ForeColor = System.Drawing.Color.Black;
-            this.change_button.Location = new System.Drawing.Point(128, 198);
+            this.change_button.Location = new System.Drawing.Point(128, 298);
             this.change_button.Name = "change_button";
             this.change_button.Size = new System.Drawing.Size(75, 23);
             this.change_button.TabIndex = 11;
@@ -73,14 +77,14 @@ namespace Log_in_Sign_up_app
             // 
             this.New_email_textBox.Location = new System.Drawing.Point(155, 111);
             this.New_email_textBox.Name = "New_email_textBox";
-            this.New_email_textBox.Size = new System.Drawing.Size(100, 20);
+            this.New_email_textBox.Size = new System.Drawing.Size(129, 20);
             this.New_email_textBox.TabIndex = 10;
             // 
             // old_email_textBox
             // 
             this.old_email_textBox.Location = new System.Drawing.Point(155, 62);
             this.old_email_textBox.Name = "old_email_textBox";
-            this.old_email_textBox.Size = new System.Drawing.Size(100, 20);
+            this.old_email_textBox.Size = new System.Drawing.Size(129, 20);
             this.old_email_textBox.TabIndex = 9;
             // 
             // label2
@@ -101,12 +105,46 @@ namespace Log_in_Sign_up_app
             this.label1.TabIndex = 7;
             this.label1.Text = "Old email";
             // 
+            // Confirme_textBox
+            // 
+            this.Confirme_textBox.Location = new System.Drawing.Point(155, 199);
+            this.Confirme_textBox.Name = "Confirme_textBox";
+            this.Confirme_textBox.Size = new System.Drawing.Size(129, 20);
+            this.Confirme_textBox.TabIndex = 14;
+            this.Confirme_textBox.Visible = false;
+            // 
+            // confirmelabel
+            // 
+            this.confirmelabel.AutoSize = true;
+            this.confirmelabel.Location = new System.Drawing.Point(64, 199);
+            this.confirmelabel.Name = "confirmelabel";
+            this.confirmelabel.Size = new System.Drawing.Size(75, 13);
+            this.confirmelabel.TabIndex = 15;
+            this.confirmelabel.Text = "Confirme code";
+            this.confirmelabel.Visible = false;
+            // 
+            // Confirme_button
+            // 
+            this.Confirme_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Confirme_button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Confirme_button.Location = new System.Drawing.Point(128, 242);
+            this.Confirme_button.Name = "Confirme_button";
+            this.Confirme_button.Size = new System.Drawing.Size(113, 23);
+            this.Confirme_button.TabIndex = 16;
+            this.Confirme_button.Text = "Confirme change";
+            this.Confirme_button.UseVisualStyleBackColor = true;
+            this.Confirme_button.Visible = false;
+            this.Confirme_button.Click += new System.EventHandler(this.Confirme_button_Click);
+            // 
             // Change_email
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(324, 248);
+            this.ClientSize = new System.Drawing.Size(324, 333);
+            this.Controls.Add(this.Confirme_button);
+            this.Controls.Add(this.confirmelabel);
+            this.Controls.Add(this.Confirme_textBox);
             this.Controls.Add(this.exist_email_label);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.change_button);
@@ -132,5 +170,8 @@ namespace Log_in_Sign_up_app
         public System.Windows.Forms.TextBox old_email_textBox;
         public System.Windows.Forms.Label label2;
         public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Confirme_textBox;
+        public System.Windows.Forms.Label confirmelabel;
+        private System.Windows.Forms.Button Confirme_button;
     }
 }
--- Log_in-Sign_up app/Change_username.Designer.cs
+++ Log_in-Sign_up app/Change_username.Designer.cs
@@ -36,12 +36,17 @@ namespace Log_in_Sign_up_app
             this.change_button = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.exist_username_label = new System.Windows.Forms.Label();
+            this.Confirme_button = new System.Windows.Forms.Button();
+            this.confirmelabel = new System.Windows.Forms.Label();
+            this.Confirme_textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 44);
+            this.label1.Location = new System.Drawing.Point(12, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 0;
@@ -50,7 +55,7 @@ namespace Log_in_Sign_up_app
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 93);
+            this.label2.Location = new System.Drawing.Point(262, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 1;
@@ -58,22 +63,22 @@ namespace Log_in_Sign_up_app
             // 
             // old_username_textBox
             // 
-            this.old_username_textBox.Location = new System.Drawing.Point(141, 44);
+            this.old_username_textBox.Location = new System.Drawing.Point(97, 53);
             this.old_username_textBox.Name = "old_username_textBox";
-            this.old_username_textBox.Size = new System.Drawing.Size(100, 20);
+            this.old_username_textBox.Size = new System.Drawing.Size(142, 20);
             this.old_username_textBox.TabIndex = 2;
             // 
             // New_username_textBox
             // 
-            this.New_username_textBox.Location = new System.Drawing.Point(141, 93);
+            this.New_username_textBox.Location = new System.Drawing.Point(347, 53);
             this.New_username_textBox.Name = "New_username_textBox";
-            this.New_username_textBox.Size = new System.Drawing.Size(100, 20);
+            this.New_username_textBox.Size = new System.Drawing.Size(142, 20);
             this.New_username_textBox.TabIndex = 3;
             // 
             // change_button
             // 
             this.change_button.ForeColor = System.Drawing.Color.Black;
-            this.change_button.Location = new System.Drawing.Point(114, 180);
+            this.change_button.Location = new System.Drawing.Point(193, 291);
             this.change_button.Name = "change_button";
             this.change_button.Size = new System.Drawing.Size(75, 23);
             this.change_button.TabIndex = 4;
@@ -93,19 +98,74 @@ namespace Log_in_Sign_up_app
             // exist_username_label
             // 
             this.exist_username_label.AutoSize = true;
-            this.exist_username_label.Location = new System.Drawing.Point(94, 137);
+            this.exist_username_label.ForeColor = System.Drawing.Color.Red;
+            this.exist_username_label.Location = new System.Drawing.Point(190, 87);
             this.exist_username_label.Name = "exist_username_label";
             this.exist_username_label.Size = new System.Drawing.Size(137, 13);
             this.exist_username_label.TabIndex = 6;
             this.exist_username_label.Text = "This username already exist";
             this.exist_username_label.Visible = false;
             // 
+            // Confirme_button
+            // 
+            this.Confirme_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Confirme_button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Confirme_button.Location = new System.Drawing.Point(193, 224);
+            this.Confirme_button.Name = "Confirme_button";
+            this.Confirme_button.Size = new System.Drawing.Size(113, 23);
+            this.Confirme_button.TabIndex = 19;
+            this.Confirme_button.Text = "Confirme change";
+            this.Confirme_button.UseVisualStyleBackColor = true;
+            this.Confirme_button.Visible = false;
+            this.Confirme_button.Click += new System.EventHandler(this.Confirme_button_Click);
+            // 
+            // confirmelabel
+            // 
+            this.confirmelabel.AutoSize = true;
+            this.confirmelabel.Location = new System.Drawing.Point(102, 181);
+            this.confirmelabel.Name = "confirmelabel";
+            this.confirmelabel.Size = new System.Drawing.Size(75, 13);
+            this.confirmelabel.TabIndex = 18;
+            this.confirmelabel.Text = "Confirme code";
+            this.confirmelabel.Visible = false;
+            // 
+            // Confirme_textBox
+            // 
+            this.Confirme_textBox.Location = new System.Drawing.Point(193, 181);
+            this.Confirme_textBox.Name = "Confirme_textBox";
+            this.Confirme_textBox.Size = new System.Drawing.Size(267, 20);
+            this.Confirme_textBox.TabIndex = 17;
+            this.Confirme_textBox.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(112, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Email:";
+            // 
+            // emailtextBox
+            // 
+            this.emailtextBox.Location = new System.Drawing.Point(198, 119);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(179, 20);
+            this.emailtextBox.TabIndex = 20;
+            // 
             // Change_username
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(325, 215);
+            this.ClientSize = new System.Drawing.Size(533, 326);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.emailtextBox);
+            this.Controls.Add(this.Confirme_button);
+            this.Controls.Add(this.confirmelabel);
+            this.Controls.Add(this.Confirme_textBox);
             this.Controls.Add(this.exist_username_label);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.change_button);
@@ -131,5 +191,10 @@ namespace Log_in_Sign_up_app
         private System.Windows.Forms.Button change_button;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label exist_username_label;
+        private System.Windows.Forms.Button Confirme_button;
+        public System.Windows.Forms.Label confirmelabel;
+        private System.Windows.Forms.TextBox Confirme_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox emailtextBox;
     }
 }
